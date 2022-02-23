8bd6487525e61612f08290ff5b6e74e245d8eeb5
JimmiJames <jimmi.james@outlook.com>
2022-02-23 21:28:08 +0530

    Removed root password from login.php

--- login.php
+++ login.php
@@ -1,7 +1,7 @@
 <?php
   $db_host = 'localhost';
   $db_user = 'root';
-  $db_password = 'root';
+  $db_password = '';
   $db_db = 'eaterometer';
  
   $conn = mysqli_connect($db_host, $db_user, $db_password, $db_db);
